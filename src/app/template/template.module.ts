eec6e8957b957d2bee3222cc450bb710fd7bdb0a
GCastro68 <gcastro2805@gmail.com>
2021-09-30 12:21:24 -0600

    Template Msg de Error

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -5,9 +5,10 @@ import { TemplateRoutingModule } from './template-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BasicosComponent, DinamicosComponent, SwitchesComponent],
-  imports: [CommonModule, TemplateRoutingModule],
+  imports: [CommonModule, TemplateRoutingModule, FormsModule],
 })
 export class TemplateModule {}
