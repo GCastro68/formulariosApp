de2cc896a92bce7217122a1b4c3838f692393bd5
GCastro68 <gcastro2805@gmail.com>
2021-10-01 20:43:31 -0600

    Fin Formulario Template

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [],
 })
-export class DinamicosComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-
+export class DinamicosComponent {
+  nuevoJuego: string = '';
+  persona: Persona = {
+    nombre: 'Gustavo',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear' },
+      { id: 2, nombre: 'DeathStranding' },
+    ],
+  };
   guardar() {
     console.log('formulario posteado');
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego,
+    };
+    this.persona.favoritos.push({ ...nuevoFavorito });
+    this.nuevoJuego = '';
   }
 }
