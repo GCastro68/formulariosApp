8a9b1c0669481a3bdaf427e9a98baf883491fd1f
GCastro68 <gcastro2805@gmail.com>
2021-10-11 11:29:55 -0600

    Validar email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidatorService } from '../../../shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
+
 import {
   FormBuilder,
   FormGroup,
@@ -12,35 +15,32 @@ import {
   styles: [],
 })
 export class RegistroComponent implements OnInit {
-  // TODO: Tempoeal
-  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
-  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
-
-  noPuedeSerGuty(control: FormControl) {
-    const valor: string = control.value?.trim().toLowerCase();
-    if (valor === 'guty') {
-      return {
-        noGuty: true,
-      };
-    }
-    return null;
-  }
-
   miFormulario: FormGroup = this.fb.group({
     nombre: [
       '',
-      [Validators.required, Validators.pattern(this.nombreApellidoPattern)],
+      [Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)],
     ],
-    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
-    username: ['', [Validators.required, this.noPuedeSerGuty]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
+    username: ['', [Validators.required, this.validatorService.noPuedeSerGuty]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+
+  }, {
+    validators: [this.validatorService.camposIguales('password', 'password2')]
   });
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorService: ValidatorService,
+    private emailValidator: EmailValidatorService
+  ) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Gustavo Castro',
-      email: 'gcastro2805@gmail.com',
+      email: 'test1@test.com',
       username: 'gcastro',
+      password: '123456',
+      password2: '123456'
     });
   }
 
@@ -49,6 +49,21 @@ export class RegistroComponent implements OnInit {
       this.miFormulario.get(campo)?.invalid &&
       this.miFormulario.get(campo)?.touched
     );
+  }
+
+  emailRequired() {
+    return this.miFormulario.get('email')?.errors?.required &&
+      this.miFormulario.get('email')?.touched;
+  }
+
+  emailFormato() {
+    return this.miFormulario.get('email')?.errors?.pattern &&
+      this.miFormulario.get('email')?.touched;
+  }
+
+  emailTomado() {
+    return this.miFormulario.get('email')?.errors?.emailTomado &&
+      this.miFormulario.get('email')?.touched;
   }
 
   submitFormulario() {
