bdccb025cba4409b3d7a89a72322109451025f63
GCastro68 <gcastro2805@gmail.com>
2021-10-11 12:19:17 -0600

    Fin Formulario

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -15,24 +15,54 @@ import {
   styles: [],
 })
 export class RegistroComponent implements OnInit {
-  miFormulario: FormGroup = this.fb.group({
-    nombre: [
-      '',
-      [Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)],
-    ],
-    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
-    username: ['', [Validators.required, this.validatorService.noPuedeSerGuty]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    password2: ['', [Validators.required]],
+  miFormulario: FormGroup = this.fb.group(
+    {
+      nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.nombreApellidoPattern),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.emailPattern),
+        ],
+        [this.emailValidator],
+      ],
+      username: [
+        '',
+        [Validators.required, this.validatorService.noPuedeSerGuty],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        this.validatorService.camposIguales('password', 'password2'),
+      ],
+    }
+  );
 
-  }, {
-    validators: [this.validatorService.camposIguales('password', 'password2')]
-  });
+  get emailErrorMsg() {
+    const errors = this.miFormulario.get('email')?.errors;
+    if (errors?.required) {
+      return 'Email es obligatorio';
+    } else if (errors?.pattern) {
+      return 'Email no tiene formato de correo';
+    } else if (errors?.emailTomado) {
+      return 'El email ya fue tomado';
+    }
+
+    return '';
+  }
   constructor(
     private fb: FormBuilder,
     private validatorService: ValidatorService,
     private emailValidator: EmailValidatorService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.miFormulario.reset({
@@ -40,7 +70,7 @@ export class RegistroComponent implements OnInit {
       email: 'test1@test.com',
       username: 'gcastro',
       password: '123456',
-      password2: '123456'
+      password2: '123456',
     });
   }
 
@@ -49,21 +79,6 @@ export class RegistroComponent implements OnInit {
       this.miFormulario.get(campo)?.invalid &&
       this.miFormulario.get(campo)?.touched
     );
-  }
-
-  emailRequired() {
-    return this.miFormulario.get('email')?.errors?.required &&
-      this.miFormulario.get('email')?.touched;
-  }
-
-  emailFormato() {
-    return this.miFormulario.get('email')?.errors?.pattern &&
-      this.miFormulario.get('email')?.touched;
-  }
-
-  emailTomado() {
-    return this.miFormulario.get('email')?.errors?.emailTomado &&
-      this.miFormulario.get('email')?.touched;
   }
 
   submitFormulario() {
